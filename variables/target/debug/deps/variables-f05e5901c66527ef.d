e23acae9cf2e5d60e0c439e1bc76541d78b9b506
Pablo Morales <pablomorales@Pablos-MacBook-Air-2.local>
2022-02-20 15:16:41 -0800

    step one

---
+++ variables/target/debug/deps/variables-f05e5901c66527ef.d
@@ -0,0 +1,5 @@
+/Users/pablomorales/RUST-CPC/variables/target/debug/deps/variables-f05e5901c66527ef: src/main.rs
+
+/Users/pablomorales/RUST-CPC/variables/target/debug/deps/variables-f05e5901c66527ef.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/pablomorales/RUST-CPC/variables/target/debug/variables: /Users/pablomorales/RUST-CPC/variables/src/main.rs
